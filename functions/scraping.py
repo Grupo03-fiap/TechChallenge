8bba329bab2e9b2efd6987867700df1b49966fcc
vsilvarios <vsilvarios@gmail.com>
2024-07-09 08:42:29 -0300

    Acrescentando o ano como parâmetro

--- functions/scraping.py
+++ functions/scraping.py
@@ -3,19 +3,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def scrap_website(url: str):
+    '''
+    Função para fazer o scraping de uma URL específica.
+    Parâmetros:
+    - url: URL da página que será feito o scraping
+    '''
     try:
+        #Configurando o Chrome
         options = Options()
         options.add_argument('--headless') #Executa o navegador sem a interface gráfica
         options.add_argument('--no-sandbox') #Desabilita um sandbox que é necessário para alguns ambientes
         options.add_argument('--disable-dev-shm-usage') #Desabilita o uso \dev necessário para alguns ambientes
 
-        # Iniciar o chrome
+        #Iniciando o Chrome
         driver = webdriver.Chrome(options = options)
+        #Abrindo URL fornecida
         driver.get(url)
+        #Coletando o conteúdo da página
         page_content = driver.page_source
+        #Fechando o Chrome
         driver.quit()
         
+        #Retornando o conteúdo da página
         return page_content
     
     except Exception as e:
+         #Em caso de erro, mostra uma exceção HTTP com código 500 e detalhe do erro
         raise HTTPException(status_code = 500, detail = str(e))
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from functions.scraping import scrap_website
 
+#Criando uma instância do FastAPI
 app = FastAPI()
 
+#Definindo um endpoint
 @app.get('/api/producao')
-def get_producao():
+def get_producao(ano: int = Query(None, description = 'Ano de produção')):
     '''
-    Endpoint para acessar os dados de produção
+    Endpoint para acessar os dados de produção.
     
+    Parâmetros:
+    - ano: ano de produção que será usado para gerar a URL.
     '''
-    website_content = scrap_website('http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02')
+    #Verificando o parâmetro "ano"
+    if ano:
+        #Se o ano for passado, monta a URL com o ano especificado
+        url = f'http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_02'
+    else:
+        #Se o ano não for passado, utiliza a URL padrão sem ano
+        url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
+
+    #Chamando a função de scraping e passando a URL
+    website_content = scrap_website(url)
+
+    #Retornando o conteúdo da página
     return website_content
