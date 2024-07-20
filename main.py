2519b2709ca7b85516157152d9dc73829d3a13cc
vsilvarios <vsilvarios@gmail.com>
2024-07-20 20:45:41 -0300

    Atualizando o código da API

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 from fastapi import FastAPI, Query
-from functions.scraping_v3 import scrape_website
+from functions.scraping import scrape_website
 
 #Criando uma instância do FastAPI
 app = FastAPI()
 
 #Definindo um endpoint
-@app.get('/api/producao')
-def get_producao(ano: int = Query(None, description = 'Ano de produção')):
+@app.get('/api/viti')
+def get_informacao(ano: int = Query(None, description = 'Ano da informação'),
+                   aba: str = Query(None, description = 'Aba (Produção/Processamento/Comercialização/Importação/Exportação)')):
     '''
-    Endpoint para acessar os dados de produção.
+    Endpoint para acessar os dados da Vitivinicultura.
     
     Parâmetros:
-    - ano: ano de produção que será usado para gerar a URL.
+    - ano: ano que será usado para gerar a URL.
+    - aba: aba que será usada para gerar a URL.
     '''
-    #Verificando o parâmetro "ano"
-    if ano:
-        #Se o ano for passado, monta a URL com o ano especificado
-        url = f'http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_02'
+    # Convertendo o texto da aba para o número presente na url
+    aba_dict = {
+        'Produção': '02',
+        'Processamento': '03',
+        'Comercialização': '04',
+        'Importação': '05',
+        'Exportação': '06'
+    }
+
+    aba_num = aba_dict.get(aba)
+
+    # Montando a URL com base nos parâmetros fornecidos
+    if ano and aba_num:
+        #Se o ano e aba forem fornecidos, monta a URL com o ano e a aba especificados
+        url = f'http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_{aba_num}'
     else:
-        #Se o ano não for passado, utiliza a URL padrão sem ano
-        url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
+        #Se o ano e a aba não forem fornecidos, utiliza a URL de apresentação
+        url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_01'
 
     #Chamando a função de scraping e passando a URL
     website_content = scrape_website(url)
