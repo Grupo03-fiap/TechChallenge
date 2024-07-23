1661b1f8d23bd3fcbbc0a236ef2ef7e2009fe4e8
vsilvarios <vsilvarios@gmail.com>
2024-07-22 23:05:00 -0300

    Atualizando o código da API

--- main.py
+++ main.py
@@ -1,41 +1,53 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from functions.scraping import scrape_website
 
 #Criando uma instância do FastAPI
 app = FastAPI()
 
-#Definindo um endpoint
-@app.get('/api/viti')
-def get_informacao(ano: int = Query(None, description = 'Ano da informação'),
-                   aba: str = Query(None, description = 'Aba (Produção/Processamento/Comercialização/Importação/Exportação)')):
-    '''
-    Endpoint para acessar os dados da Vitivinicultura.
+# Mapear o tipo para o nome da pagina na URL
+tipo_map = {
+    'producao': 'opt_02',
+    'processamento': 'opt_03',
+    'comercializacao': 'opt_04',
+    'importacao': 'opt_05',
+    'exportacao': 'opt_06',    
+}
+
+# Mapear o subtipo para o nome da pagina na URL
+subtipo_map = {
+    'viniferas': 'subopt_01',
+    'vinhodemesa': 'subopt_01',
+    'americanas': 'subopt_02',
+    'espumantes': 'subopt_02',
+    'uvasdemesa': 'subopt_03',
+    'uvasfrescas': 'subopt_03',
+    'semclassificacao': 'subopt_04',
+    'uvaspassas': 'subopt_04',
+    'sucodeuva': 'subopt_05',
+}
+
+# Definindo o endpoint
+@app.get("/api/{ano}/{tipo}")
+@app.get("/api/{ano}/{tipo}/{subtipo}")
+@app.get("/api/csv/{ano}/{tipo}")
+
+# Função
+def get_dados(ano: int, tipo: str, subtipo: str = None):
+        
+    # Construir a URL com ano e tipo
+    url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao={tipo_map[tipo]}"
     
-    Parâmetros:
-    - ano: ano que será usado para gerar a URL.
-    - aba: aba que será usada para gerar a URL.
-    '''
-    # Convertendo o texto da aba para o número presente na url
-    aba_dict = {
-        'Produção': '02',
-        'Processamento': '03',
-        'Comercialização': '04',
-        'Importação': '05',
-        'Exportação': '06'
-    }
-
-    aba_num = aba_dict.get(aba)
-
-    # Montando a URL com base nos parâmetros fornecidos
-    if ano and aba_num:
-        #Se o ano e aba forem fornecidos, monta a URL com o ano e a aba especificados
-        url = f'http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_{aba_num}'
-    else:
-        #Se o ano e a aba não forem fornecidos, utiliza a URL de apresentação
-        url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_01'
+    # Adicionar subtipo à URL, se fornecido
+    if subtipo:
+        url += f"&subopcao={subtipo_map[subtipo]}"
 
     #Chamando a função de scraping e passando a URL
-    website_content = scrape_website(url)
+    try:
+        website_content = scrape_website(url)
+        return website_content  # FastAPI converte automaticamente o dicionário Python para JSON
+    except Exception as e:
+        print(f"Erro ao executar scraping: {e}")
 
-    #Retornando o conteúdo da página
-    return website_content
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
