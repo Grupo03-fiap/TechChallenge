8ab9469f8a6e15abf0f42f27cb4889548ebc2fd9
vsilvarios <vsilvarios@gmail.com>
2024-07-25 22:50:47 -0300

    Adicionando a classe consult_aba no código main

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI
 from functions.scraping import scrape_website
+from enum import Enum
 
 #Criando uma instância do FastAPI
 app = FastAPI()
 
+class consult_aba(str, Enum):
+    producao = 'Produção'
+    processamento = 'Processamento'
+    comercializacao = "Comercialização"
+    importacao = "Importação"
+    exportacao = "Exportação"
+
 # Mapear o tipo para o nome da pagina na URL
 tipo_map = {
-    'producao': 'opt_02',
-    'processamento': 'opt_03',
-    'comercializacao': 'opt_04',
-    'importacao': 'opt_05',
-    'exportacao': 'opt_06',    
+    'Produção': 'opt_02',
+    'Processamento': 'opt_03',
+    'Comercialização': 'opt_04',
+    'Importação': 'opt_05',
+    'Exportação': 'opt_06',    
 }
 
 # Mapear o subtipo para o nome da pagina na URL
@@ -31,22 +39,26 @@ subtipo_map = {
 @app.get("/api/{ano}/{tipo}/{subtipo}")
 @app.get("/api/csv/{ano}/{tipo}")
 
-# Função
-def get_dados(ano: int, tipo: str, subtipo: str = None):
-        
+# Função de consulta os campos de pagina , ano e subtipo
+def get_dados(tipo: consult_aba,ano: int, subtipo: str = None):
+        '''para o campo de subtipo utilizar as seguintes opções
+        - viniferas; - vinhodemesa; - americanas; - espumantes; -
+         uvasdemesa; - uvasfrescas; - semclassificacao; - uvaspassas; -
+         sucodeuva;
+        '''
     # Construir a URL com ano e tipo
-    url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao={tipo_map[tipo]}"
+        url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao={tipo_map[tipo]}"
     
     # Adicionar subtipo à URL, se fornecido
-    if subtipo:
-        url += f"&subopcao={subtipo_map[subtipo]}"
+        if subtipo:
+            url += f"&subopcao={subtipo_map[subtipo]}"
 
     #Chamando a função de scraping e passando a URL
-    try:
-        website_content = scrape_website(url)
-        return website_content  # FastAPI converte automaticamente o dicionário Python para JSON
-    except Exception as e:
-        print(f"Erro ao executar scraping: {e}")
+        try:
+            website_content = scrape_website(url)
+            return website_content  # FastAPI converte automaticamente o dicionário Python para JSON
+        except Exception as e:
+            print(f"Erro ao executar scraping: {e}")
 
 if __name__ == "__main__":
     import uvicorn
