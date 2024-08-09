7bc9e4449ff5ccfd5cd432bbbe68f4d995bc5708
vsilvarios <vsilvarios@gmail.com>
2024-08-08 23:14:48 -0300

    Atualizações em main.py, scraping.py e requirements. Inclusão do plano de deploy.

--- functions/scraping.py
+++ functions/scraping.py
@@ -2,13 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import json
+import csv
+import time
+
 
 def scrape_website(url):
 
     # Configuração do driver
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    service = Service("C:/Py/API2/chromedriver.exe")
+    #service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
+    #options.add_argument('--no-sandbox')
+    #options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(service=service, options=options)
 
     # Abre a pagina
     driver.get(url)
@@ -34,7 +41,7 @@ def scrape_website(url):
             item = {headers[i]: columns[i].text for i in range(len(columns))}
             data.append(item)
             # Exibir dados para debug
-            print("Row data:", item)
+            #print("Row data:", item)
 
     # Fecha o navegador
     driver.quit()
@@ -46,3 +53,104 @@ def scrape_website(url):
     }
 
     return final_data
+
+def scrape_all(anoDe, anoAte):
+    
+    # Dicionario das paginas
+    pages_map = {
+        'producao': 'opt_02',
+        'processamento': 'opt_03',
+        'comercializacao': 'opt_04',
+        'importacao': 'opt_05',
+        'exportacao': 'opt_06',
+    }
+
+    subpages_map = {
+        'processamento': {
+            'viniferas': 'subopt_01',
+            'americanas': 'subopt_02',
+            'uvasdemesa': 'subopt_03',
+            'semclassificacao': 'subopt_04',
+        },
+        'importacao': {
+            'vinhodemesa': 'subopt_01',
+            'espumantes': 'subopt_02',
+            'uvasfrescas': 'subopt_03',
+            'uvaspassas': 'subopt_04',
+            'sucodeuva': 'subopt_05',
+        },
+        'exportacao': {
+            'vinhodemesa': 'subopt_01',
+            'espumantes': 'subopt_02',
+            'uvasfrescas': 'subopt_03',
+            'sucodeuva': 'subopt_04',
+        },
+    }
+        
+    # Configuração do driver
+    service = Service("C:/Py/API2/chromedriver.exe")
+    #service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
+    #options.add_argument('--no-sandbox')
+    #options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(service=service, options=options)
+
+    base_url = "http://vitibrasil.cnpuv.embrapa.br/index.php?ano={year}&opcao={tipo}"
+
+    with open("scraping_data.csv", mode="a", newline="", encoding="utf-8") as file:
+        csv_writer = csv.writer(file)
+        if file.tell() == 0:
+            csv_writer.writerow(["Ano", "Tipo", "Subtipo", "Cabeçalho", "Dados"])
+
+        for year in range(anoDe, anoAte + 1):
+            for tipo, tipo_value in pages_map.items():
+                if tipo in subpages_map:
+                    # Se o tipo tiver subtipos, pule o scraping da página principal
+                    for subtipo, subtipo_value in subpages_map[tipo].items():
+                        subtipo_url = f"{base_url.format(year=year, tipo=tipo_value)}&subopcao={subtipo_value}"
+                        try:
+                            driver.get(subtipo_url)
+                            driver.implicitly_wait(1)
+                            content_center = driver.find_element(By.CSS_SELECTOR, "div.content_center p.text_center").text
+                            headers = [header.text for header in driver.find_elements(By.CSS_SELECTOR, "table.tb_dados th")]
+                            table_rows = driver.find_elements(By.CSS_SELECTOR, "table.tb_dados tbody tr")
+
+                            data = []
+                            for row in table_rows:
+                                columns = row.find_elements(By.CSS_SELECTOR, "td")
+                                if columns:
+                                    item = {headers[i]: columns[i].text for i in range(len(columns))}
+                                    data.append(item)
+
+                            save_to_csv(csv_writer, year, tipo, subtipo, content_center, data)
+                        except Exception as e:
+                            print(f"Erro ao fazer scraping do ano {year}, tipo {tipo}, subtipo {subtipo}: {e}")
+                else:
+                    # Se não houver subtipos, faça o scraping da página principal
+                    url = base_url.format(year=year, tipo=tipo_value)
+                    try:
+                        driver.get(url)
+                        driver.implicitly_wait(1)
+                        content_center = driver.find_element(By.CSS_SELECTOR, "div.content_center p.text_center").text
+                        headers = [header.text for header in driver.find_elements(By.CSS_SELECTOR, "table.tb_dados th")]
+                        table_rows = driver.find_elements(By.CSS_SELECTOR, "table.tb_dados tbody tr")
+
+                        data = []
+                        for row in table_rows:
+                            columns = row.find_elements(By.CSS_SELECTOR, "td")
+                            if columns:
+                                item = {headers[i]: columns[i].text for i in range(len(columns))}
+                                data.append(item)
+
+                        save_to_csv(csv_writer, year, tipo, None, content_center, data)
+                    except Exception as e:
+                        print(f"Erro ao fazer scraping do ano {year}, tipo {tipo}: {e}")
+
+    driver.quit()
+    print("Scraping concluído e dados armazenados em scraping_data.csv.")
+
+def save_to_csv(csv_writer, year, tipo, subtipo, content, data):
+    for row in data:
+        csv_writer.writerow([year, tipo, subtipo, content, row])
+    
--- main.py
+++ main.py
@@ -1,27 +1,91 @@
-from fastapi import FastAPI
-from functions.scraping import scrape_website
-from enum import Enum
+from fastapi import FastAPI, HTTPException, Depends, Query
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+from typing import Optional, List
+from functions.scraping import scrape_website, scrape_all
+import jwt
+import csv
 
-#Criando uma instância do FastAPI
-app = FastAPI()
+# Criando uma instância do FastAPI
+app = FastAPI(
+    title="API de Scraping da EMBRAPA",
+    description="""
+Esta API permite acessar dados sobre a produção, processamento, comercialização, importação e exportação de vinhos, sucos e derivados no Brasil.
+    
+## Introdução
+Seja bem-vindo a documentação da API de consulta de dados da Vitivinicultura brasileira!
+Nossa API foi criada utilizando o padrão REST que possibilita a integração de seu sistema ao nosso, 
+sendo assim você também pode extender ou recriar as funcionalidades existentes na nossa plataforma, 
+tudo isso consumindo a API que está documentada abaixo.
 
-class consult_aba(str, Enum):
-    producao = 'Produção'
-    processamento = 'Processamento'
-    comercializacao = "Comercialização"
-    importacao = "Importação"
-    exportacao = "Exportação"
+## Autenticação
 
-# Mapear o tipo para o nome da pagina na URL
+- Para acessar os endpoints protegidos, você precisa obter um token de acesso.
+- Use o endpoint `/token` para gerar um token.
+- Inclua o token no cabeçalho `Authorization` como um Bearer Token em todas as requisições subsequentes.
+
+## Endpoints Disponíveis
+
+- `/api/{ano}/{tipo}/{subtipo}`: Consulta online de dados por ano e tipo e subtipo.
+- `/api/db/update/{anoDe}/{anoAte}`: Atualiza a base de dados offline realizando scraping de todos os anos e tipos especificados.
+- `/api/db/consulta/{ano}/{tipo}/{subtipo}`: Consulta os dados armazenados offline por ano e tipo e subtipo.
+    """,
+    version="v1.0",
+    docs_url="/docs"
+    
+)
+
+# Configuração do JWT
+SECRET_KEY = "grupoFIAP03/techChallenge2024"
+ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+# Função para criar o token JWT usando HS256
+def cria_jwt_hash():
+    to_encode = {}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# Função para verificar o token JWT
+def verifica_token(token: str, credentials_exception):
+    try:
+        jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except jwt.PyJWTError:
+        raise credentials_exception
+    return TokenData(token=token)
+
+# Função de dependência para obter o usuário atual a partir do token
+async def valida_token(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Não foi possível validar o token",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return verifica_token(token, credentials_exception)
+
+# Endpoint para gerar e retornar um token de acesso
+@app.get("/token", response_model=Token, summary="Gerar token de acesso", description="Gera um token JWT para autenticação.")
+def gerar_token():
+    access_token = cria_jwt_hash()
+    return {"access_token": access_token, "token_type": "bearer"}
+
+# Mapear o tipo para o nome da página na URL
 tipo_map = {
-    'Produção': 'opt_02',
-    'Processamento': 'opt_03',
-    'Comercialização': 'opt_04',
-    'Importação': 'opt_05',
-    'Exportação': 'opt_06',    
+    'producao': 'opt_02',
+    'processamento': 'opt_03',
+    'comercializacao': 'opt_04',
+    'importacao': 'opt_05',
+    'exportacao': 'opt_06',    
 }
 
-# Mapear o subtipo para o nome da pagina na URL
+# Mapear o subtipo para o nome da página na URL
 subtipo_map = {
     'viniferas': 'subopt_01',
     'vinhodemesa': 'subopt_01',
@@ -32,33 +96,79 @@ subtipo_map = {
     'semclassificacao': 'subopt_04',
     'uvaspassas': 'subopt_04',
     'sucodeuva': 'subopt_05',
+    'expsucodeuva': 'subopt_04',
 }
 
-# Definindo o endpoint
-@app.get("/api/{ano}/{tipo}")
-@app.get("/api/{ano}/{tipo}/{subtipo}")
-@app.get("/api/csv/{ano}/{tipo}")
-
-# Função de consulta os campos de pagina , ano e subtipo
-def get_dados(tipo: consult_aba,ano: int, subtipo: str = None):
-        '''para o campo de subtipo utilizar as seguintes opções
-        - viniferas; - vinhodemesa; - americanas; - espumantes; -
-         uvasdemesa; - uvasfrescas; - semclassificacao; - uvaspassas; -
-         sucodeuva;
-        '''
+# Endpoint de consulta online
+@app.get(
+    "/api/{ano}/{tipo}",
+    summary="Consulta online por ano e tipo",
+    description="Consulta os dados diretamente do site por ano e tipo específico, realizando o scraping ao vivo.",
+)
+@app.get(
+    "/api/{ano}/{tipo}/{subtipo}",
+    summary="Consulta online por ano, tipo e subtipo",
+    description="Consulta os dados diretamente do site por ano, tipo e subtipo específico, realizando o scraping ao vivo.",
+)
+def get_dados(ano: int, tipo: str, subtipo: str = None, current_user: TokenData = Depends(valida_token)):
     # Construir a URL com ano e tipo
-        url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao={tipo_map[tipo]}"
+    url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao={tipo_map[tipo]}"
     
     # Adicionar subtipo à URL, se fornecido
-        if subtipo:
-            url += f"&subopcao={subtipo_map[subtipo]}"
+    if subtipo:
+        url += f"&subopcao={subtipo_map[subtipo]}"
 
-    #Chamando a função de scraping e passando a URL
-        try:
-            website_content = scrape_website(url)
-            return website_content  # FastAPI converte automaticamente o dicionário Python para JSON
-        except Exception as e:
-            print(f"Erro ao executar scraping: {e}")
+    # Chamando a função de scraping e passando a URL
+    try:
+        website_content = scrape_website(url)
+        return website_content  # FastAPI converte automaticamente o dicionário Python para JSON
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao executar scraping: {e}")
+
+# Endpoint de download da base de dados para consulta offline
+@app.get(
+    "/api/db/update/{anoDe}/{anoAte}",
+    summary="Atualizar base de dados offline",
+    description="Realiza o scraping de todas as páginas de anos específicos e salva os dados em um arquivo CSV para consulta offline."
+)
+def get_csv(anoDe: int, anoAte: int, current_user: TokenData = Depends(valida_token)):
+    # Chamando a função de scraping e passando a URL
+    try:
+        scrape_all(anoDe, anoAte)
+        return {"status": "success", "message": "Scraping completed successfully."}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+# Endpoint de consulta offline
+@app.get(
+    "/api/db/consulta/{ano}/{tipo}",
+    summary="Consultar base de dados offline por ano e tipo",
+    description="Consulta os dados armazenados offline no arquivo CSV com base no ano e tipo.",
+)
+@app.get(
+    "/api/db/consulta/{ano}/{tipo}/{subtipo}",
+    summary="Consultar base de dados offline por ano, tipo e subtipo",
+    description="Consulta os dados armazenados offline no arquivo CSV com base no ano, tipo e subtipo.",
+)
+def consulta_offline(ano: int, tipo: str, subtipo: str = None, current_user: TokenData = Depends(valida_token)):
+    resultados = []
+    
+    # Abrir o arquivo CSV e filtrar os dados
+    try:
+        with open("scraping_data.csv", mode="r", newline="", encoding="utf-8") as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                if row["Year"] == str(ano) and row["Tipo"] == tipo and (subtipo is None or row["Subtipo"] == subtipo):
+                    resultados.append(row)
+        
+        if not resultados:
+            raise HTTPException(status_code=404, detail="Nenhum dado encontrado para os parâmetros fornecidos.")
+        
+        return resultados
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Arquivo de dados CSV não encontrado.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao consultar o CSV: {e}")
 
 if __name__ == "__main__":
     import uvicorn
